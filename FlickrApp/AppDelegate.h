5624582f8599ff446cc00e7840f070a576564ea4
Askar Bakirov <b.asqar@gmail.com>
2017-11-23 13:04:23 +0100

    Initial Commit

---
+++ FlickrApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrApp
+//
+//  Created by aSqar on 23.11.2017.
+//  Copyright © 2017 Askar Bakirov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
